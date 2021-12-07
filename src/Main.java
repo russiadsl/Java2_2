c0d97eaab1675915ca44efb8e804e7abb96da0cd
russiadsl <bezugly_spb@mail.ru>
2021-12-07 21:12:15 +0300

    Renew code. Separate bonus from amount

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         int count = 100;
-        int refill = 1050;
+        int refill = 10000;
         int bonus;
         if (refill > 1000) {
-            count = count + refill + (refill * 1 / 100);
-        } else count = count + refill + 0;
-        bonus = count - (refill + (refill * 1 / 100));
+            bonus = (refill / 100);
+        } else bonus = 0;
+        count = (count + refill + bonus);
         System.out.println(count);
         System.out.println(bonus);
     }
